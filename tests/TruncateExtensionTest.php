2c362573706d40f55b8fe35be5ded9fed73c33da
Daryl Gubler <daryl.gubler@va.gov>
2020-02-12 08:34:33 -0500

    Add Truncate extension and fix tests

--- tests/TruncateExtensionTest.php
+++ tests/TruncateExtensionTest.php
@@ -2,20 +2,20 @@
 
 namespace Gubler\Twig\Extension\Tests;
 
-use Gubler\Twig\Extension\InstanceOfExtension;
+use Gubler\Twig\Extension\TruncateExtension;
 use Twig\Test\IntegrationTestCase;
 
-class InstanceOfExtensionTest extends IntegrationTestCase
+class TruncateExtensionTest extends IntegrationTestCase
 {
     public function getExtensions(): array
     {
         return [
-            new InstanceOfExtension(),
+            new TruncateExtension(),
         ];
     }
 
     public function getFixturesDir(): string
     {
-        return __DIR__.'/Fixtures/InstanceOf';
+        return __DIR__.'/Fixtures/Truncate';
     }
 }
